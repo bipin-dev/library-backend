3159f3f93ebbfb84bc8caa5b6c3ba408e917e695
bipin-dev <bipps.dev@gmail.com>
2020-06-27 19:41:00 +0530

    update port number

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port: 3000,
+  port: 4000,
   app_name: "Library Management",
   version: "1.0.0",
   dir: {
@@ -8,14 +8,14 @@ module.exports = {
     actions: "/src/actions",
     routes: "/src/routes",
     navs: "/src/navs",
-    action_validator: "/src/action-validator",
+    action_validator: "/src/action-validator"
   },
   jwt: {
     audience: "https://*.bipin.io",
     subject: "anonymous",
     issuer: "library",
     algorithm: "HS256",
-    expiresIn: "1d",
+    expiresIn: "1d"
   },
-  secretJWT: "67e1d9c5af3ff740fc791354fff",
+  secretJWT: "67e1d9c5af3ff740fc791354fff"
 };
